55857e4ec83c5836e6d0b117d4fe981e438c8add
Garrett McQuigg <garrettmcquigg@gmail.com>
2023-05-25 16:16:18 -0500

    deploy

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,23 +1,23 @@
-import ip from '../images/ip-track1.jpg';
-import spotify from '../images/spotify.jpg';
-import ffl from '../images/ffl.jpg';
+// import ip from '../images/ip-track1.jpg';
+// import spotify from '../images/spotify.jpg';
+// import ffl from '../images/ffl.jpg';
 
 export default [
   {
     title: 'Fantasy Football App',
-    imgUrl: ffl,
+    // imgUrl: ffl,
     stack: ['React', 'Javascript', 'HTML', 'CSS', 'Tailwind'],
     link: 'https://portfolio-dlrfantasy.netlify.app/',
   },
   {
     title: 'Spotify Connected Application',
-    imgUrl: spotify,
+    // imgUrl: spotify,
     stack: ['NodeJS', 'Express', 'React', 'HTML', 'CSS'],
     link: 'https://spotify-gbm.herokuapp.com/',
   },
   {
     title: 'IP Tracker',
-    imgUrl: ip,
+    // imgUrl: ip,
     stack: ['Python', 'Flask', 'HTML', 'CSS'],
     link: 'https://gbm-ip-tracker.herokuapp.com/',
   },
