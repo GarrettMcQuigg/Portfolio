24e69e061a9903a747fd5bc56069d2e0695f9804
Garrett McQuigg <garrettmcquigg@gmail.com>
2023-05-27 16:36:36 -0500

    revision

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,33 +8,32 @@ function About() {
         <div className='px-8'>
           <h1 className='md:text-4xl font-bold text-gray-100 pb-4'>About Me</h1>
           <p className='text-sm indent-8 tracking-wider'>
-            My name is Garrett McQuigg and I am a 23 year old software engineer. I attended the <i>University of Arkansas</i> where I received my degree in
-            business management. Shortly after graduation, I realized that I wasn't in the correct professional field to get the most out of myself. I began
-            researching other opportunities that would not only challenge me more, but would also leave me with a great deal of pride and fulfillment.
+            My name is Garrett McQuigg and I attended the <i>University of Arkansas</i> where I received my degree in business management. Shortly after
+            graduation, I wanted to increase my value to a company or team, which led me to discover my passion for web development and software engineering.
           </p>{' '}
           <br />
           <p className='text-sm indent-8 tracking-wider'>
-            In September of 2021, I finally found my passion for software engineering, and wrote my first line of code. I started my journey as a self-taught
-            front-end developer using <i>FreeCodeCamp.org</i> to learn the basics of HTML, CSS, and Javascript. In May of 2022, I decided to increase my
-            skillset by enrolling in a Full-Stack development bootcamp through <i>Springboard Institution</i>.
+            In September of 2021, I finally wrote my first line of code. I started my journey as a self-taught front-end developer using <i>FreeCodeCamp.org</i>{' '}
+            to learn the basics of HTML, CSS, and Javascript. In May of 2022, I decided to increase my skillset by enrolling in a Full-Stack development
+            bootcamp through <i>Springboard Institution</i> to learn back-end development.
           </p>
           <br />
           <p className='text-sm indent-8 tracking-wider'>
             Through the bootcamp, as well as everything I have learned on my own, I believe I'm a very well rounded developer with the hunger to continue
-            learning more, but can also be an asset to any team that I work with.
+            learning more, but also be an asset to any team that I work with.
           </p>
           <br />
           <p className='text-sm indent-8 tracking-wider'>Here is a list of technologies I've been working with:</p>
           <ul className='grid grid-cols-3 pt-4 list-disc list-inside text-lime-500 font-semibold md:pl-12'>
             <li>Golang</li>
             <li>React</li>
-            <li>Angular</li>
-            <li>Python</li>
-            <li>Node.js</li>
-            <li>Express</li>
             <li>Flask</li>
+            <li>Node.js</li>
+            <li>Angular</li>
+            <li>Express</li>
+            <li>Python</li>
+            <li>Ruby on Rails</li>
             <li>Typescript</li>
-            <li>Javascript</li>
           </ul>
         </div>
         <div className='flex justify-center xl:justify-start lg:pl-8 pt-8 lg:pt-16 md:pl-2'>
--- src/components/ExperienceItems.jsx
+++ src/components/ExperienceItems.jsx
@@ -1,20 +1,15 @@
-import React from "react";
+import React from 'react';
 
 function ExperienceItems({ date, title, duration, details }) {
   return (
-    <ol className="flex flex-col md:flex-row relative border-l border-white">
-      <li className="mb-10 ml-4">
-        <div className="absolute w-3 h-3 bg-white rounded-full mt-1.5 -left-1.5 border border-white" />
-        <p className="flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm">
-          <span className="inline-block px-2 py-1 font-semibold text-white bg-lime-700 rounded-md">
-            {date}
-          </span>
-          <h3 className="text-lg font-bold text-white">{title}</h3>
-          <div className="my-1 text-sm font-normal leading-none text-lime-500">
-            {duration}
-          </div>
+    <ol className='flex flex-col md:flex-row relative border-l border-white'>
+      <li className='mb-10 ml-4'>
+        <div className='absolute w-3 h-3 bg-white rounded-full mt-1.5 -left-1.5 border border-white' />
+        <p className='flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm'>
+          <span className='inline-block px-2 py-1 font-semibold text-white bg-lime-700 rounded-md'>{date}</span>
+          <h3 className='text-lg font-bold text-white'>{title}</h3>
         </p>
-        <p className="my-2 text-base font-normal text-stone-100">{details}</p>
+        <p className='my-2 text-base font-normal text-stone-100'>{details}</p>
       </li>
     </ol>
   );
